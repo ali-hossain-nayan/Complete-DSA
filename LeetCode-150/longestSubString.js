abded626653b72c99b867fff2a15dd653deb2d04
Nayan1234l <ali15-13943@diu.edu.bd>
2024-09-18 12:43:28 +0600

    2-solved

---
+++ LeetCode-150/longestSubString.js
@@ -0,0 +1,53 @@
+// Given a string s, find the length of the longest 
+// substring
+//  without repeating characters.
+
+
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+// Constraints:
+
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let maxLength = 0;
+    let uniqueSet = new Set();
+    let left = 0,right = 0;
+    while (right < s.length) {
+        let currentChar = s[right];
+        if (!uniqueSet.has(currentChar)) {
+            uniqueSet.add(currentChar);
+            maxLength = Math.max(maxLength, uniqueSet.size);
+            right++;
+        }
+        else {
+            uniqueSet.delete(s[left]);
+            left++;
+        }
+    }
+    return maxLength;
+};
