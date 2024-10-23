b4957baa2e65e67f3bfa3d3b48371e11cc829b1e
Nayan1234l <ali15-13943@diu.edu.bd>
2024-10-23 13:43:53 +0600

    solved

---
+++ LeetCode-150/searchInsertPosition.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        if (target === nums[middle]) {
+            return middle;
+        }
+        if (target > nums[middle]) {
+            left = middle + 1;
+        }
+        else {
+            right = middle-1;
+        }
+    }
+    return left;
+};
